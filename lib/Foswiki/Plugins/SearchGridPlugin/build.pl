13a9cedb0f566819ab223a715717a3bf1d9368aa
Daniel Supplieth <supplieth@modell-aachen.de>
2016-09-16 11:33:29 +0200

    Fix build
    Ticket: 14057

--- lib/Foswiki/Plugins/SearchGridPlugin/build.pl
+++ lib/Foswiki/Plugins/SearchGridPlugin/build.pl
@@ -1,41 +1,15 @@
 #!/usr/bin/perl -w
-#
-# Example build class. Copy this file to the equivalent place in your
-# plugin or contrib and edit.
-#
-# Read the comments at the top of lib/Foswiki/Contrib/Build.pm for
-# details of how the build process works, and what files you
-# have to provide and where.
-#
-# Requires the environment variable FOSWIKI_LIBS (a colon-separated path
-# list) to be set to point at the build system and any required dependencies.
-# Usage: ./build.pl [-n] [-v] [target]
-# where [target] is the optional build target (build, test,
-# install, release, uninstall), test is the default.
-# Two command-line options are supported:
-# -n Don't actually do anything, just print commands
-# -v Be verbose
-#
-
-# Standard preamble
 use strict;
-
-BEGIN {
-    unshift @INC, split( /:/, $ENV{FOSWIKI_LIBS} );
-}
-
+BEGIN { unshift @INC, split(/:/, $ENV{FOSWIKI_LIBS}); }
 use Foswiki::Contrib::Build;
 
-# Declare our build package
-package SearchPluginBuild;
-use Foswiki::Contrib::Build;
-our @ISA = qw( Foswiki::Contrib::Build );
+package SearchGridBuild;
+our @ISA = qw(Foswiki::Contrib::Build);
 
 sub new {
-    my $class = shift;
-    return bless( $class->SUPER::new("SearchGridPlugin"), $class );
+  my $class = shift;
+  return bless($class->SUPER::new( "SearchGridPlugin" ), $class);
 }
-
 
 sub target_build {
   my $this = shift;
@@ -51,11 +25,5 @@ sub _installDeps {
   print $this->sys_action( qw(npm install) );
 }
 
-package main;
-
-# Create the build object
-my $build = new SearchPlugindBuild();
-
-# Build the target on the command line, or the default target
-$build->build( $build->{target} );
-
+my $build = SearchGridBuild->new();
+$build->build($build->{target});
