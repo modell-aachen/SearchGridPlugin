278f740707cb8a023a95a29f3aa1e77831511342
Andre Tebart <tebart@modell-aachen.de>
2016-09-22 15:06:29 +0200

    Refactors SearchGridPlugin javascript API.
    Ticket: 14057
    
    Release-Note: none

--- dev/main.js
+++ dev/main.js
@@ -2,9 +2,12 @@ import Vue from 'vue/dist/vue.js'
 import TopicGrid from './components/TopicGrid.vue'
 
 window.Vue = Vue;
-window.registerCustomComponent = function(name, component){
-    Vue.component(name, component);
+var SearchGridPlugin = {
+    registerField: function(name, component){
+        Vue.component(name, component);
+    }
 };
+window.SearchGridPlugin = SearchGridPlugin;
 
 $( function () {
     new Vue({
