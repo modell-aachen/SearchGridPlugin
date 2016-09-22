c614c3e7da3a49f78edd19dcd56a7c83d67dfd8c
Andre Tebart <tebart@modell-aachen.de>
2016-09-22 15:02:56 +0200

    Removes console log.
    Ticket: 14057
    
    Release-Note: none

--- dev/components/fields/DateField.vue
+++ dev/components/fields/DateField.vue
@@ -8,7 +8,6 @@ export default {
     props: ['doc','params'],
     computed: {
         date: function(){
-        	console.log(this.doc[this.params[0]]);
             return Moment(this.doc[this.params[0]], Moment.ISO_8601).toDate().toLocaleDateString();
         }
     }
