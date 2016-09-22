3fde6a80b380f118b0d2e0fc343378070be7c8c0
Andre Tebart <tebart@modell-aachen.de>
2016-09-22 16:38:40 +0200

    Removes unnecessary files.
    Ticket: 14057
    
    Release-Note: none

--- dev/tasks/config/clean.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(grunt) {
-  grunt.config.set('clean', {
-    dev: [
-      'pub/System/SearchGridPlugin/*',
-    ]
-  });
-
-  grunt.loadNpmTasks('grunt-contrib-clean');
-};
