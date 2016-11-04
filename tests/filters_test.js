7d35becfc886642f39cf3fc95dace496fe85c41b
André Tebart <tebart@modell-aachen.de>
2016-11-04 16:45:31 +0100

    Adds unit tests for the SelectFilter.
    Ticket: none

--- tests/filters_test.js
+++ tests/filters_test.js
@@ -1,6 +1,9 @@
 import Helpers from './helpers.js'
 import $ from 'jquery'
 import FullTextFilter from "../dev/components/filters/FullTextFilter.vue"
+import SelectFilter from "../dev/components/filters/SelectFilter.vue"
+
+import MockupFacetValues from "./mockup_data/facet_values.json"
 
 import './mockup_functions/jsi18n.js'
 
@@ -37,5 +40,55 @@ describe("The full-text-filter", () => {
 		fullTextFilter.filterText = "Some text";
 		fullTextFilter.$emit('clear-filters');
 		expect(fullTextFilter.filterText).toBe("");
+	});
+});
+
+describe("The select-filter", () => {
+	let selectFilter = Helpers.createVueComponent(SelectFilter, {
+		el : () => {return 'body'},
+		replace: false,
+		propsData: {
+			facetValues: MockupFacetValues,
+			params: ["TitleText", "language"]
+		}
+	});
+
+	it("should have the correct ID", () => {
+		expect(selectFilter.id).toBe("TitleText_filter");
+	});
+
+	it('should have a dummy empty totalCount value', () => {
+		expect(selectFilter.totalCount).toBe("");
+	});
+
+	it('should be flagged as default if selected option is the empty string', () => {
+		selectFilter.selectedOption = '';
+		expect(selectFilter.isDefault).toBe(true);
+	});
+
+	it('should not be flagged as default if selected option is not empty', () => {
+	  selectFilter.selectedOption = 'en';
+	  expect(selectFilter.isDefault).toBe(false);
+	});
+
+	it('should reset the selected option when receiving a clear-filters message', () => {
+	  selectFilter.selectedOption = 'en';
+	  selectFilter.$emit('clear-filters');
+	  expect(selectFilter.selectedOption).toBe('');
+	});
+
+	it('should update the selected facet value when the selected option is changed', () => {
+	  selectFilter.selectedOption = 'de';
+	  selectFilter.$nextTick(() => {
+	  	expect(selectFilter.selectedFacet[0].field).toBe('de');
+	  	selectFilter.selectedOption = "en";
+	  	selectFilter.$nextTick(() => {
+	  		expect(selectFilter.selectedFacet[0].field).toBe("en");
+	  		selectFilter.selectedOption = "";
+	  		selectFilter.$nextTick(() => {
+		  		expect(selectFilter.selectedFacet.length).toBe(0);
+		  	});
+	  	});
+	  });
 	});
 });
