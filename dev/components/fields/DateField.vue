d6514ccf1b4201ad172a002dd80a985367338857
Daniel Supplieth <supplieth@modell-aachen.de>
2017-03-28 17:47:17 +0200

    Add relative date to date field.
    id: 446
    Ticket: none
    Release-Note(feature, appdev) {
        Add relative date to date field.
    }

--- dev/components/fields/DateField.vue
+++ dev/components/fields/DateField.vue
@@ -11,6 +11,9 @@ export default {
             let date = this.doc[this.params[0]];
             if(typeof date === 'undefined')
                 return "";
+            if(this.params[1]) {
+                return moment(date, moment.ISO_8601).fromNow(true);
+            }
             return moment(date, moment.ISO_8601).toDate().toLocaleDateString();
         }
     }
