020d6e6aa8831d7cbc0ba6f011bedf9a88bd328f
Andre Tebart <tebart@modell-aachen.de>
2017-04-19 13:36:18 +0200

    Fixes an UrlField bug when provided url is undefined.
    Ticket: none
    
    Release-Note: none

--- dev/components/fields/UrlField.vue
+++ dev/components/fields/UrlField.vue
@@ -11,6 +11,8 @@ export default {
 		//Used to parse an solrField, which is not an URL to an URL
 		parsedUrl: function() {
 			var url = this.doc[this.params[1]];
+			if(!url)
+				return "";
 			if(url.substring(0,1) == "/") {
 				return url;
 			} else {
